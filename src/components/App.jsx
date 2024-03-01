b56a4d871a3b9293e13fa6d209c9da28c1aa15fd
Matthew <lehgran@gmail.com>
2024-03-01 16:28:44 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,18 @@ export class App extends Component {
   filter: '',
 }
 
+    componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts?.length) {
+      this.setState({ contacts });
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+}  }
+  
  handleAddContact = ({ name, number }) => {
     const { contacts } = this.state;
 
